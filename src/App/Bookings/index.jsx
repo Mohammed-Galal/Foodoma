c1f5ac8de9961fa408483b97c9196c76fad9959d
Mohammed Galal <mohammedgalal682@gmail.com>
2024-11-02 22:39:18 +0200

    countdown timer

--- src/App/Bookings/index.jsx
+++ src/App/Bookings/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { categories } from "../Home/Products";
 import "./index.scss";
 
@@ -7,17 +8,21 @@ const O = categories.is_new.map((C) => (
   </div>
 ));
 
+const expireDate = new Date();
+
+// setting Fixed Time (currTime(ms) + expireTime(ms))
+expireDate.setTime(expireDate.getTime() + 172800000);
+
 export default (
   <>
     <section id="book-banner">
       <div className="container align-items-stretch d-flex">
         <div className="align-items-center d-flex flex-column py-5">
           <span className="h1 my-5">كيكات اليوم الوطني السعودي</span>
-
           <div className="text-center">
             ينتهي الحجز في
-            <p className="d-grid my-3">
-              {/* <CountDown until="" /> */}
+            <p className="d-grid my-4">
+              <Timer />
               <span>دقيقة</span>:<span>ساعة</span>:<span>يوم</span>
             </p>
             خصم 15% على الحجز المبكر لكيكات اليوم الوطني
@@ -37,6 +42,26 @@ export default (
       className="align-items-stretch container d-grid justify-content-center"
     >
       {O}
+      {O}
     </section>
   </>
 );
+
+function Timer() {
+  const [update, setUpdate] = useState(true);
+
+  const now = new Date().getTime(),
+    seconds = (expireDate - now) / 1000,
+    minutes = seconds / 60,
+    houres = minutes / 60;
+
+  setTimeout(() => setUpdate(!update), 1000);
+
+  return (
+    <span className="align-items-baseline display-4 timer">
+      <samp>{Math.floor(seconds % 60)}</samp>:
+      <samp>{Math.floor(minutes % 60)}</samp>:
+      <samp>{Math.floor(houres % 24)}</samp>
+    </span>
+  );
+}
